8db89c30a2f43d159c755a3fb438736150508fb3
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2022-10-11 10:52:14 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("Let's get this party started!");
+
+
+// element selectors
+const navbar = document.querySelector("#navBar");
+const textInput = document.getElementById('input');
+const submitButton = document.querySelector('#Submitbutton');
+const resetButton = document.querySelector('#resetbutton');
+
+// variables
+const testPrint = function(){console.log('hi this is a test')};
+
+// event listeners
+submitButton.addEventListener("click", ( i => (console.log(getSearchTerm()))));
+
+
+async function getSearchTerm () {
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${textInput.value}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    console.log(response);
+    return response.data;
+}
+
+
+
+
+
+
+
+// button event listener
+// document.body.addEventListener("keypress", function(){
+//     if (event.key === "Enter"){
+//         console.log(textInput.value);
+//         getSearchTerm();
+//     }
+
+// });

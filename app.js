c2dd462fbf7c1595e328a8d10af8ec0543a5a829
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2022-10-17 16:35:27 -0400

    Competed project

--- app.js
+++ app.js
@@ -6,31 +6,37 @@ const navbar = document.querySelector("#navBar");
 const textInput = document.getElementById('input');
 const submitButton = document.querySelector('#Submitbutton');
 const resetButton = document.querySelector('#resetbutton');
-
-// variables
-const testPrint = function(){console.log('hi this is a test')};
+const gifArea = document.getElementById('gifs');
+const newGifs = document.getElementsByClassName("newGif");
 
 // event listeners
-submitButton.addEventListener("click", ( i => (console.log(getSearchTerm()))));
+submitButton.addEventListener("click", (i => (console.log(getSearchTerm()))));
+resetButton.addEventListener("click", (i => (gifArea.remove(newGifs), textInput.value = '')))
 
 
-async function getSearchTerm () {
+async function getSearchTerm() {
+    // in-function variables
+    const numGen = Math.floor(Math.random() * 50);
+    const img = document.createElement('img');
+    const span = document.createElement('span');
+    // can I make this line w/params? I know this isnt pretty
     let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${textInput.value}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-    console.log(response);
+    // get random gif function
+    function getRandom(x) {
+        const selector = response.data.data[x];
+        return selector;
+    }
+    const gifSelector = getRandom(numGen);
+    const gifID = gifSelector.id;
+    const gifURL = `https://media4.giphy.com/media/${gifID}/giphy.gif`;
+    function insertTest() {
+        gifArea.prepend(span);
+        span.classList.add("newGif");
+        span.appendChild(img);
+        img.src = gifURL;
+        return;
+    }
+    insertTest();
     return response.data;
 }
 
-// ll
-
-
-
-
-
-// button event listener
-// document.body.addEventListener("keypress", function(){
-//     if (event.key === "Enter"){
-//         console.log(textInput.value);
-//         getSearchTerm();
-//     }
-
-// });
